ebc393003773119d5d41ea2c950e234289df986e
Renn-11 <elrenohasani@gmail.com>
2024-09-03 12:36:57 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,17 @@
+print("Hello World")
+
+num1 = 900
+num2 = 12
+perkalian = num1 * num2
+pangkat = num1 ** num2
+kurang = num1 - num2
+tambah = num1 + num2 
+modulo = num1 % num2
+
+print (num1)
+print (num2)
+print (perkalian)
+print (pangkat)
+print (kurang)
+print (tambah)
+print (modulo)
